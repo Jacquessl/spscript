f0a6162e08cb63ca089fc62e88b2bf18aec946f6
Jacquessl <97961605+Jacquessl@users.noreply.github.com>
2024-01-06 21:48:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 (async () => {
+  let mapa = new Map();
+  mapa.set("Skrzynia Pryzmatu 2", 2.4);
+  mapa.set("Skrzynia Załamania", 1.4);
+  mapa.set("Skrzynia Rewolucji", 2);
+  mapa.set("Skrzynia Snów i koszmarów", 3);
+  mapa.set("Skrzynia Ukąszenia Węża", 0.7);
+  mapa.set("Skrzynia Odrzutu", 0.8);
+  mapa.set("Skrzynia Clutch", 1.3);
+  mapa.set("Skrzynia Strefy zagrożenia", 1.9);
+  mapa.set("Skrzynia Pryzmatu", 1.9);
+  mapa.set("Skrzynia Spektrum 2", 4.2);
+  mapa.set("Skrzynia Horyzontu", 2.4);
+  mapa.set("Skrzynia CS20", 1.8);
+  mapa.set("Skrzynia Falcjonu", 2.4);
+  mapa.set("Skrzynia Cienia", 2.4);
+  let suma = 0;
+  let last = "";
+  let kupujemy = false;
+  let lastPrice = 0;
+  let setDodanych = new Set();
     while (true) {
       const offset = Math.random() + 1.2;
       await new Promise(r => setTimeout(r, 100));
@@ -6,21 +26,41 @@
         const itemContainers = document.querySelectorAll('div.CatalogPage-item--grid');
   
         for (const container of itemContainers) {
+          const nameElement = container.querySelector('div.ItemPreview-itemName');
+          const curName = nameElement.textContent;
           const priceElement = container.querySelector('div.ItemPreview-priceValue');
           const discountElement = container.querySelector('div.GradientLabel.ItemPreview-discount');
-  
+
           if (priceElement && discountElement) {
             const currentPrice = parseFloat(priceElement.textContent.replace("zł", "").replace(",", "."));
             const discountMatch = discountElement.textContent.match(/\d+/);
-  
-            if (discountMatch) {
-              const discountValue = parseInt(discountMatch[0]);
-  
-              if (currentPrice > 10 && discountValue >= 30) {
+            if(mapa.get(curName) > currentPrice){
+              if(!setDodanych.has(currentPrice)){
                 const addToCartButton = container.querySelector('button.ItemPreview-mainAction');
                 if (addToCartButton) {
                   addToCartButton.click();
+                }
+                suma+=currentPrice;
+                last = curName;
+                kupujemy = false;
+                lastPrice = currentPrice;
+              }
+            }
+            if(last!=curName && suma > 10){
+              kupujemy = true;
+            }
+            if(last!=curName){
+              setDodanych.add(lastPrice);
+            }
+            if (discountMatch) {
+              const discountValue = parseInt(discountMatch[0]);
   
+              if ((currentPrice > 10 && discountValue >= 30) || kupujemy) {
+                const addToCartButton = container.querySelector('button.ItemPreview-mainAction');
+                if (addToCartButton) {
+                  if(!kupujemy){
+                    addToCartButton.click();
+                  }
                   while (true) {
                     const offset = Math.random() + 1.2;
                     await new Promise(r => setTimeout(r, 100 * offset));
@@ -32,31 +72,31 @@
                         await new Promise(r => setTimeout(r, 500)); // Wait for the cart to open
   
                         const checkoutButton = document.querySelector('div.CartDropdown-btns button.CartDropdown-checkout');
+
+
                         if (checkoutButton) {
                           checkoutButton.click();
   
-                          // Poczekaj na zmianę URL, oznaczającą przejście do kolejnej strony
                           await new Promise(resolve => {
                             const observer = new MutationObserver(() => {
                               if (window.location.href.includes('https://skinport.com/pl/cart')) {
-  observer.disconnect();
+                                observer.disconnect();
                                 resolve();
                               }
                             });
                             observer.observe(document.body, { childList: true, subtree: true });
                           });
   
-                          // Po załadowaniu nowej strony dodaj kliknięcie w checkboxy
                           const checkboxToClick1 = document.querySelector('[id^="cb-tradelock-"]');
                           const checkboxToClick2 = document.querySelector('[id^="cb-cancellation-"]');
   
                           if(!checkboxToClick1){
-          checkboxToClick2.click();
-  }else{
-                            checkboxToClick1.click();
-                            checkboxToClick2.click();
-  }
-          const checkoutBtn = document.querySelector('button.SubmitButton.CartSummary-checkoutBtn');
+                                  checkboxToClick2.click();
+                          }else{
+                                                    checkboxToClick1.click();
+                                                    checkboxToClick2.click();
+                          }
+                          const checkoutBtn = document.querySelector('button.SubmitButton.CartSummary-checkoutBtn');
                           if (checkoutBtn) {
                             checkoutBtn.click();
                  
