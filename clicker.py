1ab2a3480ffbbd095cee2ea6c19a494ecc306c05
Jacquessl <97961605+Jacquessl@users.noreply.github.com>
2024-01-09 20:56:12 +0100

    Create clicker.py

---
+++ clicker.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+while True:
+    try:
+        pozycja = pyautogui.locateOnScreen('cvv.png')
+        if pozycja is not None:
+            pyautogui.moveTo(pozycja)
+            pyautogui.click()
+            pyautogui.write('425')
+            pyautogui.moveTo(1040, 453)
+            pyautogui.click()
+        else:
+            time.sleep(0.05)
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
