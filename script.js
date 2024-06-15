d5ebcd225f644f16ea310d5f82df6082a5464589
Jacquessl <97961605+Jacquessl@users.noreply.github.com>
2024-06-15 11:25:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 (async () => {
   let mapa = new Map();
-  mapa.set("Skrzynia Pryzmatu 2", 1.76);
-  mapa.set("Skrzynia Załamania", 0.62);
-  mapa.set("Skrzynia Rewolucji", 0.79);
-  mapa.set("Skrzynia Snów i koszmarów", 2.43);
+  mapa.set("Skrzynia Pryzmatu 2", 1.75);
+  mapa.set("Skrzynia Załamania", 0.59);
+  mapa.set("Skrzynia Rewolucji", 0.93);
+  mapa.set("Skrzynia Snów i koszmarów", 2.77);
   mapa.set("Skrzynia Ukąszenia Węża", 0.59);
   mapa.set("Skrzynia Odrzutu", 0.42);
-  mapa.set("Skrzynia Clutch", 1.09);
-  mapa.set("Skrzynia Strefy zagrożenia", 1.69);
-  mapa.set("Skrzynia Pryzmatu", 1.66);
-  mapa.set("Skrzynia Horyzontu", 2.21);
-  mapa.set("Skrzynia CS20", 1.39);
+  mapa.set("Skrzynia Clutch", 1.17);
+  mapa.set("Skrzynia Strefy zagrożenia", 1.68);
+  mapa.set("Skrzynia Pryzmatu", 1.56);
+  mapa.set("Skrzynia Horyzontu", 2.25);
+  mapa.set("Skrzynia CS20", 1.35);
   mapa.set("Skrzynia Falcjonu", 1.94);
-  mapa.set("Skrzynia Cienia", 2.07);
+  mapa.set("Skrzynia Cienia", 2.2);
   mapa.set("Skrzynia operacji Shattered Web", 5)
   let suma = 0;
   let last = "";
