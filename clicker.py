be4c50911592dc924222cacfa2c6d57ed86d5ac1
Jacquessl <97961605+Jacquessl@users.noreply.github.com>
2024-08-07 17:32:54 +0200

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -3,14 +3,15 @@ import time
 
 while True:
     try:
-        pozycja = pyautogui.locateOnScreen('cvv.png')
+        pozycja = pyautogui.locateOnScreen('cvv.png', region=(900, 400, 120, 60))
         if pozycja is not None:
+            pozycja = (950, 460)
             pyautogui.moveTo(pozycja)
             pyautogui.click()
-            pyautogui.write()
-            pyautogui.moveTo(1040, 453)
+            pyautogui.typewrite()
+            pyautogui.moveTo(910, 560)
             pyautogui.click()
         else:
             time.sleep(0.05)
     except Exception as e:
-        print(f"Wystąpił błąd: {e}")
+        pass
